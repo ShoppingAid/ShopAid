90c316783ea828d6ab7e294ec23c765ccec58cd9
ShoppingAid <63202997+ShoppingAid@users.noreply.github.com>
2020-04-05 22:37:27 +0530

    Update places_auto.py

--- places_auto.py
+++ places_auto.py
@@ -3,5 +3,5 @@ import populartimes
 place_id=["ChIJC4fvXa8EdkgRQX1Mjup8vOQ","ChIJFXqFpOYcdkgR-c7TYfZuGhM"]
 paces=len(place_id)
 for i in range(0,paces):
-	place_details=populartimes.get_id("AIzaSyDCXtBtHwAN9r_quMJ7f1WSHvGN6cGtPEQ",place_id[i])
+	place_details=populartimes.get_id("API Key",place_id[i])
 	print(place_details)
