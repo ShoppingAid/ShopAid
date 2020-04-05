fc9e986e16809418009d470d2ac6379bba2461a4
ShoppingAid <63202997+ShoppingAid@users.noreply.github.com>
2020-04-05 22:36:53 +0530

    Update places.py

--- places.py
+++ places.py
@@ -1,9 +1,9 @@
 from googlemaps import * 
 import populartimes
 
-tesco_timing=populartimes.get_id("AIzaSyDCXtBtHwAN9r_quMJ7f1WSHvGN6cGtPEQ","ChIJC4fvXa8EdkgRQX1Mjup8vOQ")
+tesco_timing=populartimes.get_id("API Key","ChIJC4fvXa8EdkgRQX1Mjup8vOQ")
 
-lidl_timing=populartimes.get_id("AIzaSyDCXtBtHwAN9r_quMJ7f1WSHvGN6cGtPEQ","ChIJFXqFpOYcdkgR-c7TYfZuGhM")
+lidl_timing=populartimes.get_id("API Key","ChIJFXqFpOYcdkgR-c7TYfZuGhM")
 
 print(tesco_timing)
 print(lidl_timing)
